bd91a863a6c5506ccb227267fda5d20936d84ae1
Lukas Fittl <lukas@fittl.com>
2015-01-25 22:31:06 +0100

    Adjust update.sh so it can run on OSX as well.

--- update.sh
+++ update.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
-
 versions=( "$@" )
 if [ ${#versions[@]} -eq 0 ]; then
 	versions=( */ )
@@ -10,16 +8,16 @@ fi
 versions=( "${versions[@]%/}" )
 
 packagesUrl='http://apt.postgresql.org/pub/repos/apt/dists/wheezy-pgdg/main/binary-amd64/Packages'
-packages="$(echo "$packagesUrl" | sed -r 's/[^a-zA-Z.-]+/-/g')"
+packages='postgres-packages.tmp'
 curl -sSL "${packagesUrl}.bz2" | bunzip2 > "$packages"
 
 for version in "${versions[@]}"; do
-	fullVersion="$(grep -m1 -A10 "^Package: postgresql-$version\$" "$packages" | grep -m1 '^Version: ' | cut -d' ' -f2)"
+	fullVersion="$(grep -A10 "^Package: postgresql-$version\$" "$packages" | grep -m1 '^Version: ' | cut -d' ' -f2)"
 	(
 		set -x
 		cp docker-entrypoint.sh Dockerfile.template "$version/"
 		mv "$version/Dockerfile.template" "$version/Dockerfile"
-		sed -i 's/%%PG_MAJOR%%/'$version'/g; s/%%PG_VERSION%%/'$fullVersion'/g' "$version/Dockerfile"
+		sed -i '' 's/%%PG_MAJOR%%/'$version'/g; s/%%PG_VERSION%%/'$fullVersion'/g' "$version/Dockerfile"
 	)
 done
 
